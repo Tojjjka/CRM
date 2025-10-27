056307f72b977ccceea050c8ab4f532ca45f189d
Anton Bykov <hantikz@mail.ru>
2025-10-27 22:41:44 +0300

    Добавили временные теги создания для клиентов и компаний

--- crm_project/crm_project/settings.py
+++ crm_project/crm_project/settings.py
@@ -103,11 +103,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
